07275cebd6fe39665fa3c635cdc465dac04d9e15
YassineElazzati <yassine.elazzati@outlook.fr>
2023-09-28 16:53:29 +0200

    experiment : services add Diagramme D'interactivité

--- src/app/services/diagrammes-uml.service.ts
+++ src/app/services/diagrammes-uml.service.ts
@@ -10,90 +10,261 @@ export class DiagrammeUmlsService{
     diagrammesUmls: DiagrammeUml[] = [
         {
             id: 1,   
+
             title: "diagramme d'interactivité.",
-            imageUrl: "",
-            description: "Un diagramme d'interactivité est un dessin ou une image qui montre comment différentes parties d'un logiciel ou d'un système informatique se parlent et se donnent des instructions. Imaginez-le comme une carte qui indique qui parle à qui et ce qu'ils se disent.Il existe deux types principaux de ces diagrammes :- Diagramme de Séquence : C'est comme une bande dessinée qui raconte l'histoire de comment les parties d'un logiciel se parlent étape par étape, comme si vous suivez un personnage à travers une série d'actions. -Diagramme de Communication : C'est comme un schéma qui montre qui est connecté à qui, comme un réseau social où vous pouvez voir qui est ami avec qui, mais ici, il montre comment les parties d'un système se connectent et se parlent.En bref, ces diagrammes d'interactivité aident les programmeurs et les concepteurs à comprendre comment les différentes parties d'un logiciel ou d'un système informatique travaillent ensemble pour accomplir des tâches. C'est un peu comme dessiner un plan pour montrer comment les gens se parlent et coopèrent pour accomplir quelque chose.",
+            imageUrl: "https://images.edrawmax.com/examples/sequence-diagram-examples/example03.jpg",
+            description: "Un diagramme d'interactivité, en particulier dans le contexte de UML (Unified Modeling Language), n'est pas un diagramme standard dans UML. Cependant, vous pouvez faire référence à plusieurs types de diagrammes UML qui représentent des interactions entre les différents composants d'un système logiciel. Les deux principaux diagrammes d'interaction en UML sont le diagramme de séquence et le diagramme de communication. Je vais expliquer ces deux types de diagrammes :",
+            title2:"Diagramme de Séquence :",
+            paragraphe1: " Qu'est-ce que c'est ? :" + 
+            " Un diagramme de séquence est un type de diagramme UML qui représente la séquence d'actions ou de messages échangés entre les objets ou les composants d'un système dans le temps." +
+            "Comment ça fonctionne ? : "+
+            "Sur un diagramme de séquence, les objets sont représentés par des rectangles verticaux, et les messages (actions ou interactions) sont représentés par des flèches qui relient les objets dans l'ordre chronologique."+
+            "Quand l'utiliser ? :"+
+            "Utilisez un diagramme de séquence pour montrer comment les objets interagissent les uns avec les autres dans un scénario donné. Il est utile pour représenter des scénarios de comportement ou des séquences d'opérations. ", 
+            title3:"",
+            paragraphe2:"",
+            title4:"",
+            paragraphe3:"",
+            title5:"",
+            paragraphe4:"",
+            title6:"",
+            paragraphe5:"",
+            conclusion:"En bref, ces diagrammes d'interactivité aident les programmeurs et les concepteurs à comprendre comment les différentes parties d'un logiciel ou d'un système informatique travaillent ensemble pour accomplir des tâches. C'est un peu comme dessiner un plan pour montrer comment les gens se parlent et coopèrent pour accomplir quelque chose."
+                
         },
         {
             id: 2,
      
-            title: "Diagramme de classe.",
+            title: "diagramme d'interactivité.",
             imageUrl: "",
-            description: "Le diagram de classe consiste a montrer les blocs de construction de tous système orienté objet. Le diagram de classe et souvent le plus utile pour illuster les relations entre les classes et les interfaces."
+            description: "Un diagramme d'interactivité, en particulier dans le contexte de UML (Unified Modeling Language), n'est pas un diagramme standard dans UML. Cependant, vous pouvez faire référence à plusieurs types de diagrammes UML qui représentent des interactions entre les différents composants d'un système logiciel. Les deux principaux diagrammes d'interaction en UML sont le diagramme de séquence et le diagramme de communication. Je vais expliquer ces deux types de diagrammes :",
+            title2:"",
+            paragraphe1:"", 
+            title3:"",
+            paragraphe2:"",
+            title4:"",
+            paragraphe3:"",
+            title5:"",
+            paragraphe4:"",
+            title6:"",
+            paragraphe5:"",
+            conclusion:""
         },
         {
             id: 3,
-            title: "Diagrammes d'Objets.",
+
+            title: "diagramme d'interactivité.",
             imageUrl: "",
-            description: "Le diagramme d'objets utilisent un sous ensemble d'élément d'un diagramme de classes afin de souligner la relation entre les instances de classe à un certain moment. Cela est utile pour la compréhension des diagramme de classe."
+            description: "Un diagramme d'interactivité, en particulier dans le contexte de UML (Unified Modeling Language), n'est pas un diagramme standard dans UML. Cependant, vous pouvez faire référence à plusieurs types de diagrammes UML qui représentent des interactions entre les différents composants d'un système logiciel. Les deux principaux diagrammes d'interaction en UML sont le diagramme de séquence et le diagramme de communication. Je vais expliquer ces deux types de diagrammes :",
+            title2:"",
+            paragraphe1:"", 
+            title3:"",
+            paragraphe2:"",
+            title4:"",
+            paragraphe3:"",
+            title5:"",
+            paragraphe4:"",
+            title6:"",
+            paragraphe5:"",
+            conclusion:""
         },
         {
             id: 4,
 
-            title: "Diagrammes de Structure Composites.",
+            title: "diagramme d'interactivité.",
             imageUrl: "",
-            description: "Le diagramme de structure composite est un diagramme qui permet d'afficher une structure interne d'un classificateur. Elle affiche la configuration et la relation de parties qui ensemble effectue le comportement d'un classificateur."
+            description: "Un diagramme d'interactivité, en particulier dans le contexte de UML (Unified Modeling Language), n'est pas un diagramme standard dans UML. Cependant, vous pouvez faire référence à plusieurs types de diagrammes UML qui représentent des interactions entre les différents composants d'un système logiciel. Les deux principaux diagrammes d'interaction en UML sont le diagramme de séquence et le diagramme de communication. Je vais expliquer ces deux types de diagrammes :",
+            title2:"",
+            paragraphe1:"", 
+            title3:"",
+            paragraphe2:"",
+            title4:"",
+            paragraphe3:"",
+            title5:"",
+            paragraphe4:"",
+            title6:"",
+            paragraphe5:"",
+            conclusion:""
         },
         {
             id: 5,
 
-            title: "Diagrammes de Composants.",
+            title: "diagramme d'interactivité.",
             imageUrl: "",
-            description: "Le Diagram de composant illustre toute les parties logiciel. Le diagramme de composant à un niveau d'abstraction plus élevé qu'un diagramme de classes "
+            description: "Un diagramme d'interactivité, en particulier dans le contexte de UML (Unified Modeling Language), n'est pas un diagramme standard dans UML. Cependant, vous pouvez faire référence à plusieurs types de diagrammes UML qui représentent des interactions entre les différents composants d'un système logiciel. Les deux principaux diagrammes d'interaction en UML sont le diagramme de séquence et le diagramme de communication. Je vais expliquer ces deux types de diagrammes :",
+            title2:"",
+            paragraphe1:"", 
+            title3:"",
+            paragraphe2:"",
+            title4:"",
+            paragraphe3:"",
+            title5:"",
+            paragraphe4:"",
+            title6:"",
+            paragraphe5:"",
+            conclusion:""
         },
         {
             id: 6,
-            title: "Diagrammes de Déploiement",
+
+            title: "diagramme d'interactivité.",
             imageUrl: "",
-            description: "Le diagramme de déploiement et un modèle architectural de temp d'exécution d'un système. Il permet d'afficher la configuration des éléments matériel (noeud) et affiche des élement logiciels."
+            description: "Un diagramme d'interactivité, en particulier dans le contexte de UML (Unified Modeling Language), n'est pas un diagramme standard dans UML. Cependant, vous pouvez faire référence à plusieurs types de diagrammes UML qui représentent des interactions entre les différents composants d'un système logiciel. Les deux principaux diagrammes d'interaction en UML sont le diagramme de séquence et le diagramme de communication. Je vais expliquer ces deux types de diagrammes :",
+            title2:"",
+            paragraphe1:"", 
+            title3:"",
+            paragraphe2:"",
+            title4:"",
+            paragraphe3:"",
+            title5:"",
+            paragraphe4:"",
+            title6:"",
+            paragraphe5:"",
+            conclusion:""
         },
         {
             id: 7,
-            title: "Diagrammes de cas d'Utilisation.",
+            
+            title: "diagramme d'interactivité.",
             imageUrl: "",
-            description: "Le modèle de cas d'utilisation capture les exigences d'un système. Il montre l'interaction entre le système et les entités externes au système. Le cas d'utilisation sont un moyen de communiquer avec les utilisateur et d'autre parties prenantes ce que le système est destiné a faire "
+            description: "Un diagramme d'interactivité, en particulier dans le contexte de UML (Unified Modeling Language), n'est pas un diagramme standard dans UML. Cependant, vous pouvez faire référence à plusieurs types de diagrammes UML qui représentent des interactions entre les différents composants d'un système logiciel. Les deux principaux diagrammes d'interaction en UML sont le diagramme de séquence et le diagramme de communication. Je vais expliquer ces deux types de diagrammes :",
+            title2:"",
+            paragraphe1:"", 
+            title3:"",
+            paragraphe2:"",
+            title4:"",
+            paragraphe3:"",
+            title5:"",
+            paragraphe4:"",
+            title6:"",
+            paragraphe5:"",
+            conclusion:""
         },
         {
             id: 8,
-            title: " Diagrammes d'activité",
+
+            title: "diagramme d'interactivité.",
             imageUrl: "",
-            description: "Le Diagrammes d'activité est utilisé pour afficher la séquence d'activité. Il affiche le flux de travail d'un point de départ à un point d'arrivée en détaillant sur les nombreux chemins de décision existant. Il peux être utilisé pour détailler des situations dans lesquelles un traitement parralèle peut avoir lieu lors d'une exécution"
+            description: "Un diagramme d'interactivité, en particulier dans le contexte de UML (Unified Modeling Language), n'est pas un diagramme standard dans UML. Cependant, vous pouvez faire référence à plusieurs types de diagrammes UML qui représentent des interactions entre les différents composants d'un système logiciel. Les deux principaux diagrammes d'interaction en UML sont le diagramme de séquence et le diagramme de communication. Je vais expliquer ces deux types de diagrammes :",
+            title2:"",
+            paragraphe1:"", 
+            title3:"",
+            paragraphe2:"",
+            title4:"",
+            paragraphe3:"",
+            title5:"",
+            paragraphe4:"",
+            title6:"",
+            paragraphe5:"",
+            conclusion:""
         },
         {
             id: 9,
-            title: " Diagrammes de StateMachine",
+
+            title: "diagramme d'interactivité.",
             imageUrl: "",
-            description: "Le Diagramme de StateMachine permet de voir le comportement d'un objet unique , en précisant la séquence des évenements qu'un objet passe pendant ca durée de vie"
+            description: "Un diagramme d'interactivité, en particulier dans le contexte de UML (Unified Modeling Language), n'est pas un diagramme standard dans UML. Cependant, vous pouvez faire référence à plusieurs types de diagrammes UML qui représentent des interactions entre les différents composants d'un système logiciel. Les deux principaux diagrammes d'interaction en UML sont le diagramme de séquence et le diagramme de communication. Je vais expliquer ces deux types de diagrammes :",
+            title2:"",
+            paragraphe1:"", 
+            title3:"",
+            paragraphe2:"",
+            title4:"",
+            paragraphe3:"",
+            title5:"",
+            paragraphe4:"",
+            title6:"",
+            paragraphe5:"",
+            conclusion:""
         },
         {
             id: 10,
-            title: " Diagrammes de communication",
+
+            title: "diagramme d'interactivité.",
             imageUrl: "",
-            description: "Le diagramme de communication est un schéma d'interaction. Il affiche des information similaire à des diagrammes de séquence, mais son objectif principal est de relations objet."
+            description: "Un diagramme d'interactivité, en particulier dans le contexte de UML (Unified Modeling Language), n'est pas un diagramme standard dans UML. Cependant, vous pouvez faire référence à plusieurs types de diagrammes UML qui représentent des interactions entre les différents composants d'un système logiciel. Les deux principaux diagrammes d'interaction en UML sont le diagramme de séquence et le diagramme de communication. Je vais expliquer ces deux types de diagrammes :",
+            title2:"",
+            paragraphe1:"", 
+            title3:"",
+            paragraphe2:"",
+            title4:"",
+            paragraphe3:"",
+            title5:"",
+            paragraphe4:"",
+            title6:"",
+            paragraphe5:"",
+            conclusion:""
         },
         {
             id: 11,
-            title: "Diagrammes de séquence",
+
+            title: "diagramme d'interactivité.",
             imageUrl: "",
-            description: "Le Diagramme de séquence est une forme de diagrammr d'interaction."
+            description: "Un diagramme d'interactivité, en particulier dans le contexte de UML (Unified Modeling Language), n'est pas un diagramme standard dans UML. Cependant, vous pouvez faire référence à plusieurs types de diagrammes UML qui représentent des interactions entre les différents composants d'un système logiciel. Les deux principaux diagrammes d'interaction en UML sont le diagramme de séquence et le diagramme de communication. Je vais expliquer ces deux types de diagrammes :",
+            title2:"",
+            paragraphe1:"", 
+            title3:"",
+            paragraphe2:"",
+            title4:"",
+            paragraphe3:"",
+            title5:"",
+            paragraphe4:"",
+            title6:"",
+            paragraphe5:"",
+            conclusion:""
         },
         {
             id: 12,
-            title: " Diagrammes de temps",
+
+            title: "diagramme d'interactivité.",
             imageUrl: "",
-            description: "Le Diagramme de temps est utilisé pour afficher le changement de valeur d'un ou plusieur élement au fil du temps. Il peux également montrer l'interaction entre les événements prévus et le temps et les contraintes de durée. "
+            description: "Un diagramme d'interactivité, en particulier dans le contexte de UML (Unified Modeling Language), n'est pas un diagramme standard dans UML. Cependant, vous pouvez faire référence à plusieurs types de diagrammes UML qui représentent des interactions entre les différents composants d'un système logiciel. Les deux principaux diagrammes d'interaction en UML sont le diagramme de séquence et le diagramme de communication. Je vais expliquer ces deux types de diagrammes :",
+            title2:"",
+            paragraphe1:"", 
+            title3:"",
+            paragraphe2:"",
+            title4:"",
+            paragraphe3:"",
+            title5:"",
+            paragraphe4:"",
+            title6:"",
+            paragraphe5:"",
+            conclusion:""
         },
         {
             id: 13,
-            title: " Diagrammes d'apercu d'interaction",
+
+            title: "diagramme d'interactivité.",
             imageUrl: "",
-            description: "Le diagramme d'apercu d'interaction est une forme d'un diagramme d'activité. Dans lequel les noueds représentent des diagrammes d'interaction. Les vues d'ensemble sont par exemple : initiale, finale, décision , fusionner, fourchette."
+            description: "Un diagramme d'interactivité, en particulier dans le contexte de UML (Unified Modeling Language), n'est pas un diagramme standard dans UML. Cependant, vous pouvez faire référence à plusieurs types de diagrammes UML qui représentent des interactions entre les différents composants d'un système logiciel. Les deux principaux diagrammes d'interaction en UML sont le diagramme de séquence et le diagramme de communication. Je vais expliquer ces deux types de diagrammes :",
+            title2:"",
+            paragraphe1:"", 
+            title3:"",
+            paragraphe2:"",
+            title4:"",
+            paragraphe3:"",
+            title5:"",
+            paragraphe4:"",
+            title6:"",
+            paragraphe5:"",
+            conclusion:""
         },
         {
             id: 14,
-            title: " Diagrammes de Profil",
-            imageUrl: "	",
-            description: "Le Diagramme de Profil fournit un mécanisme d'éxtenstion générique pour construire des modèles UML dans des domaines particuliers. Il sont fondés sur les stéréotypes et les valeurs. "
+
+            title: "diagramme d'interactivité.",
+            imageUrl: "",
+            description: "Un diagramme d'interactivité, en particulier dans le contexte de UML (Unified Modeling Language), n'est pas un diagramme standard dans UML. Cependant, vous pouvez faire référence à plusieurs types de diagrammes UML qui représentent des interactions entre les différents composants d'un système logiciel. Les deux principaux diagrammes d'interaction en UML sont le diagramme de séquence et le diagramme de communication. Je vais expliquer ces deux types de diagrammes :",
+            title2:"",
+            paragraphe1:"", 
+            title3:"",
+            paragraphe2:"",
+            title4:"",
+            paragraphe3:"",
+            title5:"",
+            paragraphe4:"",
+            title6:"",
+            paragraphe5:"",
+            conclusion:""
         }
     ];
 
