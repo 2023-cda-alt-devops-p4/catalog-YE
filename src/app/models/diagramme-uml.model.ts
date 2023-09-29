9f90e8bb5573d1efea2c567d658a79842f70feb8
YassineElazzati <yassine.elazzati@outlook.fr>
2023-09-29 14:10:33 +0200

    experiment : services add new uml

--- src/app/models/diagramme-uml.model.ts
+++ src/app/models/diagramme-uml.model.ts
@@ -1,18 +1,7 @@
 export class DiagrammeUml{
     id!:number;
      title!: string;
-    title2?: string;
-    title3?: string;
-    title4?: string;
-    title5?:string;
-    title6?:string;
     description!: string;
     imageUrl!:string;
-    paragraphe1?:string;
-    paragraphe2?:string;
-    paragraphe3?:string;
-    paragraphe4?:string;
-    paragraphe5?:string;
-    conclusion?:string;
-
+  
 }
