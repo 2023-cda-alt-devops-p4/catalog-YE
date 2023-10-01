5fac9e076c42711d75fecdd99b84fa0e716fad7c
YassineElazzati <yassine.elazzati@outlook.fr>
2023-10-01 10:35:34 +0200

    experiment : services with new UML and Method Merise

--- src/app/models/diagramme-uml.model.ts
+++ src/app/models/diagramme-uml.model.ts
@@ -1,7 +1,8 @@
 export class DiagrammeUml{
     id!:number;
-     title!: string;
+    title!: string;
     description!: string;
+    content?: string;
     imageUrl!:string;
     imageUrl2?:string;
     imageUrl3?:string;
